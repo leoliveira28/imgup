53b3c6b1b7bf0866c4ab169038134b39e92e1d20
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-04-27 21:06:49 -0300

    test error

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,23 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+const  { onOpen, isOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+const [ currentUrl, setCurrentUrl] = useState('');
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentUrl(url)
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleViewImage}/>
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} imgUrl={currentUrl} onClose={onClose}/>
     </>
   );
 }
